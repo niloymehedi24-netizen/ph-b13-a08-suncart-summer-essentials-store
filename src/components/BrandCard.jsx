f1a146edc2c9b36849e979f79bb9d3efb72cb60e
hasanniloy122-rgb <hasanniloy122@gmail.com>
2026-05-10 02:24:25 +0600

    added popular products section

--- src/components/BrandCard.jsx
+++ src/components/BrandCard.jsx
@@ -1,19 +1,46 @@
-import { Card } from "@heroui/react";
+import { Button, Card } from "@heroui/react";
 import Image from "next/image";
+import { FcRating } from "react-icons/fc";
+import { IoIosPricetags } from "react-icons/io";
 import React from "react";
 
 const BrandCard = ({ brand }) => {
   console.log(brand);
   return (
-    <Card>
-      <div>
+    <Card className=" max-w-7xl mx-auto border rounded-xl m-10">
+      <div className="relative w-full aspect-square">
         <Image
           src={brand.image}
-          height={300}
-          width={300}
+          fill
           alt={brand.name}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          className="object-cover rounded-xl"
         ></Image>
       </div>
+      <div>
+        <h2 className="font-medium">{brand.name}</h2>
+      </div>
+      <div className="flex justify-between">
+        <div className="flex gap-1 items-center">
+          <p className="font-medium">
+            <FcRating></FcRating>
+          </p>
+          <p className="font-medium">{brand.rating}</p>
+        </div>
+        <div className="flex gap-1 items-center">
+          <p className="font-medium">
+            <IoIosPricetags></IoIosPricetags>
+          </p>
+          <p className="font-bold text-xl text-green-600">${brand.price}</p>
+        </div>
+      </div>
+      <Button
+        className={
+          "w-full font-bold bg-linear-to-r from-yellow-500 via-orange-500 bg-red-500"
+        }
+      >
+        View Details
+      </Button>
     </Card>
   );
 };
--- src/components/TopBrands.jsx
+++ src/components/TopBrands.jsx
@@ -6,13 +6,13 @@ const TopBrands = async () => {
     "https://ph-b13-a08-suncart-summer-essential.vercel.app/data.json",
   );
   const data = await res.json();
-  const topBrands = data.slice(0, 6);
+  const topBrands = data.slice(0, 3);
   console.log(topBrands);
 
   return (
     <div className="max-w-7xl mx-auto">
-      <h1 className="text-3xl font-bold mt-5">Top Brands</h1>
-      <div>
+      <h1 className="text-3xl font-bold mt-5">Popular Products</h1>
+      <div className="grid grid-cols-3 gap-5">
         {topBrands.map((brand) => (
           <BrandCard key={brand.id} brand={brand}></BrandCard>
         ))}
